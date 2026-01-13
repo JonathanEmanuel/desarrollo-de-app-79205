2c2ed61a00f2b0cde1ffb17d7b89c92ce77bb939
Jonathan Cruz <cruzjonathan.e@gmail.com>
2026-01-13 20:58:19 -0300

    Unidad 02 - Estilos y Listas

--- unidad-02/App.js
+++ unidad-02/App.js
@@ -1,16 +1,54 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button, TextInput} from 'react-native';
 
 export default function App() {
+
+  const [ textItem, setTextItem ] = useState('estudiar JS');
+  const [ itemList, setItemList ] = useState([]);
+
+  const onHandlerChangeItem = (t) => {
+    setTextItem(t)
+  }
+
+  const addItem = () => {
+    if( textItem.trim() === ''){
+      return;
+    }
+
+    const task = {
+      id: Math.random().toString(),
+      value: textItem
+    }
+    setItemList( prev => [ ...prev, task ]  );
+    setTextItem('');
+
+    console.log('Agregando Tarea');
+  }
+  
   return (
     <View style={styles.container}>
 
       <View style={styles.tarea}>
-        <TextInput style={ styles.input} placeholder='Tarea' />
-        <Button title='Nueva' />
+        <TextInput 
+          style={ styles.input} 
+          placeholder='Tarea...' 
+          value={textItem}
+          onChangeText={ onHandlerChangeItem }
+          />
+        <Button title='Nueva' onPress={addItem} />
       </View>
       <Text style={{ padding: 50, color: 'teal', fontWeight: 900 }}>Hola Coder!</Text>
-      
+      <View>
+
+        { 
+          itemList.map( item =>
+            <view style={styles.itemList}>  
+              <Text>{ item.value }</Text> 
+            </view>  )
+        }
+
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -25,6 +63,12 @@ const styles = StyleSheet.create({
   input: {
     width: '50%',
     borderColor: '#999',
+    borderWidth: 1,
+    padding: 10,
+  },
+  itemList: {
+    backgroundColor: '#1adbed',
+    margin: 2,
     borderWidth: 1,
     padding: 10,
   },
