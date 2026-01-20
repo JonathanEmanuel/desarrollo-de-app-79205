bb9d4ac191abe4b01402945ff276d7d7d3e68b87
Jonathan Cruz <cruzjonathan.e@gmail.com>
2026-01-20 20:08:28 -0300

    Unidad 03

---
+++ unidad-03/README.md
@@ -0,0 +1,8 @@
+# 🎯 Objetivos de la clase
+
+- Utilizar Core Componentes para la construcción del Layout.
+- Generar estilos en los componentes.
+- Incluir Layout y Flexbox para organizar la posición de los elementos.
+- Utilizar eventos y estados con Hooks.
+- Optimizar la performance de elementos de lista.
+- Manejar eventos de un modal.
---
+++ unidad-03/components/ModalTask.js
@@ -0,0 +1,55 @@
+import { StyleSheet, Modal, View, Text, Button } from "react-native";
+
+
+const ModalTask = ( props) => {
+    const { modalVisible, onCerrar, onFinalizar, id, title, date } = props;
+
+    return (
+        <Modal visible={modalVisible} transparent animationType="fade">
+            <View style={ styles.modalContainer}>
+                <View style={ styles.modalContent}>
+                    <Text style={ styles.modalTitle} > Soy un Componente Modal</Text>
+                    <Text> Fecha</Text>
+                </View>
+                <View style={styles.modalFooter}>
+                    <Button 
+                        title="Cerrar" 
+                        onPress={onCerrar} 
+                    />
+                    <Button title="Finalizar" onPress={onFinalizar} />
+
+                </View>
+            </View>
+
+        </Modal>
+    )
+
+}
+
+const styles = StyleSheet.create({
+    modalContainer: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 10,
+        width: '100%',
+        backgroundColor: '#16161673'
+    },
+    modalTitle: {
+        fontSize: 26,
+        fontWeight: '600'
+    },
+    modalContent: {
+        backgroundColor: '#FFF',
+        borderRadius: 10,
+        padding: 10,
+        justifyContent: 'space-between',
+    },
+    modalFooter: {
+        flexDirection: 'row',
+        gap: 10
+
+    }
+})
+
+export default ModalTask
